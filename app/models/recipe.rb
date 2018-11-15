8b91010ad7cfa5d5a8d1685caa3bed1c05e79601
Joe Guerra <guerra_joe@hotmail.com>
2018-11-15 09:43:17 -0800

    ..

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_attached_file :image, styles: { :medium => "400x400" }
   validates :title, :description, :image, presence: true
-  validates_attachment_content_type :image, content_type: /\Aimage
-
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
     has_many :ingredients
     has_many :directions
 
